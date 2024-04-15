eed74cdd3f6f64ab3699fa0351e49535c5b5799e
Anggi <ortegacanillo76@gmail.com>
2024-04-15 16:08:10 +0800

    allow all origins

--- app/router.go
+++ app/router.go
@@ -9,6 +9,20 @@ import (
 )
 
 func MapUrls() {
+
+	// allow all origins
+	router.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
 	router.GET("/", func(c *gin.Context) {
 		c.IndentedJSON(200, gin.H{
 			"status": "ok",
