d2036a5ad4587d50acb33514bd5163e43eb673b3
Anggi <ortegacanillo76@gmail.com>
2024-04-15 11:14:39 +0800

    update adde dwebhook

--- app/router.go
+++ app/router.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+
 	"anggi.tabulation/controllers/posts"
 	"anggi.tabulation/controllers/users"
 	"github.com/gin-gonic/gin"
@@ -9,7 +13,18 @@ import (
 func MapUrls() {
 	router.GET("/", func(c *gin.Context) {
 		c.IndentedJSON(200, gin.H{
-			"message": "Welcome to the API",
+			"status": "ok",
+		})
+	})
+
+	router.POST("/webhook", func(c *gin.Context) {
+		body, _ := io.ReadAll(c.Request.Body)
+		println(string(body))
+
+		c.Request.Body = io.NopCloser(bytes.NewReader(body))
+		c.IndentedJSON(200, gin.H{
+			"status":   "ok",
+			"response": json.RawMessage(body),
 		})
 	})
 
